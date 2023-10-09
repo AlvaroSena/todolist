bc3d735e447d0876f4ecf13b8b2c914437350341
alvarosena <alvarosenacs.c@gmail.com>
2023-10-09 16:17:50 -0300

    first controller using spring annotations

---
+++ src/test/java/br/com/alvarosena/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alvarosena.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
